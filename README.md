e9449cc32dcf0368ebd443bc9d0cdc3ef1c7be49
Tipriest <a1503741059@163.com>
2025-06-17 22:52:18 +0800

    modified:   README.md modified:   ch5_boids/boids.py

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # README.md
-本仓库是Books仓库的一部分，系对《Python极客项目编程》这本书的工程内容的复现
+本仓库是Books仓库的一部分，系对《Python极客项目编程》这本书的工程内容的复现。
+## chapter5 boids(模拟鱼群运动)
+
+<div align="center">
+<img src="https://cdn.jsdelivr.net/gh/Tipriest/blog-images@main/imgs/my_blog/2025%2F06%2F17%2F050c73433f43bdad00d13ec372c1620f-20250617224723507.png" width="400" />
+</div>
+
+## chapter15 music spider(音乐文件爬虫)
--- ch5_boids/boids.py
+++ ch5_boids/boids.py
@@ -14,35 +14,40 @@ import matplotlib.animation as animation
 from scipy.spatial.distance import squareform, pdist, cdist
 from numpy.linalg import norm
 
+# 定义画布的宽度和高度
 width, height = 640, 480
 
 class Boids:
-    """Class that represents Boids simulation"""
+    """表示Boids仿真的类"""
     def __init__(self, N):
-        """ initialize the Boid simulation"""
-        # init position & velocities
+        """ 初始化Boid仿真 """
+        # 初始化位置和速度
+        # 位置初始化在画布中心附近, 加入一定的随机扰动
         self.pos = [width/2.0, height/2.0] + 10*np.random.rand(2*N).reshape(N, 2)
-        # normalized random velocities
+        # 随机生成角度, 初始化速度为单位向量
         angles = 2*math.pi*np.random.rand(N)
         self.vel = np.array(list(zip(np.sin(angles), np.cos(angles))))
         self.N = N
-        # min dist of approach
+        # 最小接近距离
         self.minDist = 25.0
-        # max magnitude of velocities calculated by "rules"
+        # 规则计算得到的最大速度
         self.maxRuleVel = 0.03
-        # max maginitude of final velocity
+        # 最终速度的最大值
         self.maxVel = 2.0
 
     def tick(self, frameNum, pts, beak):
-        """Update the simulation by one time step."""
-        # get pairwise distances
+        """仿真推进一步, 更新所有Boid的位置和速度"""
+        # 计算所有Boid之间的距离矩阵
         self.distMatrix = squareform(pdist(self.pos))
-        # apply rules:
+        # 应用三条规则, 更新速度
         self.vel += self.applyRules()
+        # 限制速度不超过最大值
         self.limit(self.vel, self.maxVel)
+        # 更新位置
         self.pos += self.vel
+        # 应用边界条件
         self.applyBC()
-        # update data
+        # 更新绘图数据
         pts.set_data(self.pos.reshape(2*self.N)[::2], 
                      self.pos.reshape(2*self.N)[1::2])
         vec = self.pos + 10*self.vel/self.maxVel
@@ -50,44 +55,44 @@ class Boids:
                       vec.reshape(2*self.N)[1::2])
 
     def limitVec(self, vec, maxVal):
-        """limit magnitide of 2D vector"""
+        """限制2D向量的大小"""
         mag = norm(vec)
         if mag > maxVal:
             vec[0], vec[1] = vec[0]*maxVal/mag, vec[1]*maxVal/mag
     
     def limit(self, X, maxVal):
-        """limit magnitide of 2D vectors in array X to maxValue"""
+        """限制数组X中2D向量的大小不超过maxValue"""
         for vec in X:
             self.limitVec(vec, maxVal)
             
     def applyBC(self):
-        """apply boundary conditions"""
+        """应用边界条件"""
         deltaR = 2.0
         for coord in self.pos:
             if coord[0] > width + deltaR:
                 coord[0] = - deltaR
             if coord[0] < - deltaR:
-                coord[0] = width + deltaR    
+                coord[0] = width + deltaR
             if coord[1] > height + deltaR:
                 coord[1] = - deltaR
             if coord[1] < - deltaR:
                 coord[1] = height + deltaR
     
     def applyRules(self):
-        # apply rule #1 - Separation
+        # 应用规则 #1 - 分离
         D = self.distMatrix < 25.0
         vel = self.pos*D.sum(axis=1).reshape(self.N, 1) - D.dot(self.pos)
         self.limit(vel, self.maxRuleVel)
 
-        # different distance threshold
+        # 不同的距离阈值
         D = self.distMatrix < 50.0
 
-        # apply rule #2 - Alignment
+        # 应用规则 #2 - 对齐
         vel2 = D.dot(self.vel)
         self.limit(vel2, self.maxRuleVel)
         vel += vel2;
 
-        # apply rule #1 - Cohesion
+        # 应用规则 #1 - 聚合
         vel3 = D.dot(self.pos) - self.pos
         self.limit(vel3, self.maxRuleVel)
         vel += vel3
@@ -95,25 +100,25 @@ class Boids:
         return vel
 
     def buttonPress(self, event):
-        """event handler for matplotlib button presses"""
-        # left click - add a boid
+        """matplotlib按钮按下事件的处理函数"""
+        # 左键点击 - 添加一个boid
         if event.button is 1:
             self.pos = np.concatenate((self.pos, 
                                        np.array([[event.xdata, event.ydata]])), 
                                       axis=0)
-            # random velocity
+            # 随机速度
             angles = 2*math.pi*np.random.rand(1)
             v = np.array(list(zip(np.sin(angles), np.cos(angles))))
             self.vel = np.concatenate((self.vel, v), axis=0)
             self.N += 1 
-        # right click - scatter
+        # 右键点击 - 散射
         elif event.button is 3:
-            # add scattering velocity 
+            # 添加散射速度 
             self.vel += 0.1*(self.pos - np.array([[event.xdata, event.ydata]]))
         
 def tick(frameNum, pts, beak, boids):
     #print frameNum
-    """update function for animation"""
+    """动画更新函数"""
     boids.tick(frameNum, pts, beak)
     return pts, beak
 
